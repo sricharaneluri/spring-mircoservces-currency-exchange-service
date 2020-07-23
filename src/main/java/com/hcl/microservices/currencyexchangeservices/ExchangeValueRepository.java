3242302225cb86f95415bc36d6edfb8b57bb9542
cherryz21 <charansri21@gmail.com>
2020-07-23 11:15:09 -0400

    currency-exchange-services

---
+++ src/main/java/com/hcl/microservices/currencyexchangeservices/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.hcl.microservices.currencyexchangeservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends 
+		JpaRepository<ExchangeValue, Long>{
+	ExchangeValue findByFromAndTo(String from, String to);
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(11001,'USD','INR',65,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(11002,'EUR','INR',75,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(11003,'AUD','INR',50,0);
